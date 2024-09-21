302c6a0af9404c24b73aaee8e606fd21b0957f86
rexicals23 <rexicala@gmail.com>
2024-09-21 06:57:55 +0330

    Update library react-router-dom

--- IRNFT/src/App.js
+++ IRNFT/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import 'font-awesome/css/font-awesome.min.css';
 import Navbar from './components/Navbar';
 import LandingSection from './components/LandingSection';
